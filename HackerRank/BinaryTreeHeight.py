8d1a082150f1792f9ba589a28ae5032f26c87c91
Cheyenne Atapour <cheyenneatapour@gmail.com>
2019-07-25 13:14:26 +0200

    Added BinaryTreeHeight
    https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=trees

---
+++ HackerRank/BinaryTreeHeight.py
@@ -0,0 +1,73 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+def height(root):
+    count = 0
+    l = [(root, 0)] # list of tuples: (node, height)
+    while len(l) > 0:
+        temp = l.pop()
+        if not temp[0].left == None:
+            l.append((temp[0].left, temp[1]+1))
+            if temp[1] + 1 > count:
+                count = temp[1] + 1
+        if not temp[0].right == None:
+            l.append((temp[0].right, temp[1]+1))
+            if temp[1] + 1 > count:
+                count = temp[1] + 1
+    return count
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
