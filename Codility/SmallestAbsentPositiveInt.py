63df13661324ab7ce42a1477bbb96f8620126e65
Cheyenne Atapour <cheyenneatapour@gmail.com>
2019-07-31 18:44:04 +0200

    Added SmallestPositiveInt
    given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A. - Codility Demo Test - 100% Solution

---
+++ Codility/SmallestAbsentPositiveInt.py
@@ -0,0 +1,9 @@
+def solution(A):
+    # write your code in Python 3.6
+    A.sort()
+    temp = 1
+    for x in range(len(A)):
+        if A[x] == temp:
+            temp += 1
+    return temp
+    pass
